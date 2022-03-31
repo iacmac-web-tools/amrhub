2afd5db030df8e2f288b54d1c6feccf53b279ed6
rymbln <rymbln@gmail.com>
2022-03-31 09:27:48 +0000

    added team

--- js/freelancer.js
+++ js/freelancer.js
@@ -9,7 +9,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 32
+            scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 4;
+const CACHE_VERSION = 5;
 
 const BASE_CACHE_FILES = [
   "/manifest.json",
